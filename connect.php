6b20292b656945c98efabf1af73e5d8586a5e51b
Shafi Noor <161951351+habib-sherfy@users.noreply.github.com>
2024-05-07 23:24:20 +0300

    Connect to database

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'test';
+$databaseUsername = '';
+$databasePassword = 'crud_operations';
+
+// Open a new connection to the MySQL server
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+`
